64178d51c0b28e656c9e0d1e905bf51734aae07f
Metzger-PC <henry.metzgermeister@outlook.com>
2023-02-15 00:01:23 +0100

    created document domain type

---
+++ Notino.Homework/Domain/Document.cs
@@ -0,0 +1,7 @@
+﻿namespace Notino.Homework.Domain;
+public class Document
+{
+    public string Id { get; init; } = string.Empty;
+    public List<string> Tags { get; init; } = new();
+    public Dictionary<string, string> Data { get; init; } = new();
+}
