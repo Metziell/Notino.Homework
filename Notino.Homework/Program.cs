3797c93e42f1df0cff239029e882764ed93d2834
Metzger-PC <henry.metzgermeister@outlook.com>
2023-02-14 22:22:53 +0100

    added logging and exception handler middleware

--- Notino.Homework/Program.cs
+++ Notino.Homework/Program.cs
@@ -1,4 +1,9 @@
+using System.Net;
+
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+
+using Newtonsoft.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddLogging(builder => builder.AddConsole());
 
 var app = builder.Build();
 
@@ -17,6 +24,37 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseExceptionHandler(builder =>
+{
+    builder.Run(async context =>
+    {
+        context.Response.ContentType = "application/json";
+
+        var exceptionHandlerFeature = context.Features.Get<IExceptionHandlerFeature>();
+        if (exceptionHandlerFeature is not null)
+        {
+            var exception = exceptionHandlerFeature.Error;
+            var logger = context.RequestServices.GetRequiredService<ILogger>();
+            logger.LogError(
+                exception,
+                "{RequestMethod} {RequestPath} {RequestQuery}: {Message}",
+                context.Request.Method,
+                context.Request.Path,
+                context.Request.QueryString,
+                exception.Message);
+
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError; 
+            var response = JsonConvert.SerializeObject(new
+            {
+                errorCode = exception.GetType().Name,
+                message = exception.Message
+            });
+
+            await context.Response.WriteAsync(response);
+        }
+    });
+});
 
 var documentsGroup = app.MapGroup("documents").WithOpenApi();
 
