46fa76e0662ad17726c60e056e44ce9005f977fa
Metzger-PC <henry.metzgermeister@outlook.com>
2023-02-15 00:03:44 +0100

    created exceptions

---
+++ Notino.Homework/Domain/Exceptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Notino.Homework.Domain;
+
+public class DuplicateDocumentException : Exception
+{
+	public DuplicateDocumentException(string documentId) : base($"A document with id {documentId} already exists.")
+	{ }
+}
+
+public class MissingDocumentException : Exception
+{
+	public MissingDocumentException(string documentId) : base($"A document with id {documentId} doesn't exists.")
+	{ }
+}
