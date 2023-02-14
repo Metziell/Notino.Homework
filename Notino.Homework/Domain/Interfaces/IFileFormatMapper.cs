77d6793e55bda121ef9d0458798ac32d240f4fb1
Metzger-PC <henry.metzgermeister@outlook.com>
2023-02-15 00:03:24 +0100

    added mapper from request Content-Type to FileFormat

---
+++ Notino.Homework/Domain/Interfaces/IFileFormatMapper.cs
@@ -0,0 +1,7 @@
+﻿using Notino.Homework.Domain;
+
+namespace Notino.Homework.Domain.Interfaces;
+public interface IFileFormatMapper
+{
+    FileFormat Map(string format);
+}
---
+++ Notino.Homework/Services/FileFormatMapper.cs
@@ -0,0 +1,14 @@
+﻿using Notino.Homework.Domain;
+using Notino.Homework.Domain.Interfaces;
+
+namespace Notino.Homework.Services;
+
+public class FileFormatMapper : IFileFormatMapper
+{
+    public FileFormat Map(string format) => format switch
+    {
+        "application/json" => FileFormat.Json,
+        "application/xml" => FileFormat.Xml,
+        _ => throw new NotImplementedException($"{nameof(format)}: {format}")
+    };
+}
