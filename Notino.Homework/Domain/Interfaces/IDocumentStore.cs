0d5cd343388a5c48dc62805bd50a7051cd0f8c92
Metzger-PC <henry.metzgermeister@outlook.com>
2023-02-15 00:02:48 +0100

    added in memory document store

---
+++ Notino.Homework/Domain/Interfaces/IDocumentStore.cs
@@ -0,0 +1,8 @@
+﻿namespace Notino.Homework.Domain.Interfaces;
+
+public interface IDocumentStore
+{
+    Task<Document?> GetDocument(string id);
+    Task InsertDocument(Document document);
+    Task UpdateDocument(Document document);
+}
---
+++ Notino.Homework/Infrastructure/InMemoryStore.cs
@@ -0,0 +1,49 @@
+﻿using Notino.Homework.Domain;
+using Notino.Homework.Domain.Interfaces;
+
+namespace Notino.Homework.Infrastructure;
+
+public class InMemoryStore : IDocumentStore
+{
+    private readonly List<Document> documents = new();
+    private readonly object lockObject = new();
+
+    public Task<Document?> GetDocument(string id)
+    {
+        lock (lockObject)
+        {
+            return Task.FromResult(documents.FirstOrDefault(x => x.Id == id));
+        }
+    }
+
+    public Task InsertDocument(Document document)
+    {
+        lock (lockObject)
+        {
+            var existing = documents.FirstOrDefault(x => x.Id == document.Id);
+            if (existing != null)
+            {
+                throw new DuplicateDocumentException(document.Id);
+            }
+
+            documents.Add(document);
+            return Task.CompletedTask;
+        }
+    }
+
+    public Task UpdateDocument(Document document)
+    {
+        lock (lockObject)
+        {
+            var index = documents.FindIndex(x => x.Id == document.Id);
+            if (index == -1)
+            {
+                throw new MissingDocumentException(document.Id);
+            }
+
+            documents[index] = document;
+
+            return Task.CompletedTask;
+        }
+    }
+}
