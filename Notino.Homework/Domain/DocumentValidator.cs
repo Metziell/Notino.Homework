e94d6468aaf34a02371f3b59ad32760423be1c6a
Metzger-PC <henry.metzgermeister@outlook.com>
2023-02-15 00:04:13 +0100

    added document validator

---
+++ Notino.Homework/Domain/DocumentValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Notino.Homework.Domain;
+
+public class DocumentValidator : AbstractValidator<Document>
+{
+	public DocumentValidator()
+	{
+		RuleFor(x => x.Id).NotEmpty();
+	}
+}
